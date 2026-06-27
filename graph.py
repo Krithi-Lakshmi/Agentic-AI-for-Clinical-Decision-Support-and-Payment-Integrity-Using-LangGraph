e053a387fe29d18c0f7266e996521fb4bbfc5e4d
Krithi-Lakshmi <162471418+Krithi-Lakshmi@users.noreply.github.com>
2026-06-27 18:52:22 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,21 +1,18 @@
 from langgraph.graph import StateGraph, END
 from langgraph.checkpoint.memory import MemorySaver
-
-from agents import rag_agent, risk_agent, fraud_agent, decision_agent
 from state import ClaimState
+from graph_nodes import rag_node, risk_node, decision_node
 
 workflow = StateGraph(ClaimState)
 
-workflow.add_node("rag", rag_agent)
-workflow.add_node("risk", risk_agent)
-workflow.add_node("fraud", fraud_agent)
-workflow.add_node("decision", decision_agent)
+workflow.add_node("rag", rag_node)
+workflow.add_node("risk", risk_node)
+workflow.add_node("decision", decision_node)
 
 workflow.set_entry_point("rag")
 
 workflow.add_edge("rag", "risk")
-workflow.add_edge("risk", "fraud")
-workflow.add_edge("fraud", "decision")
+workflow.add_edge("risk", "decision")
 workflow.add_edge("decision", END)
 
 checkpointer = MemorySaver()
